6470fa1830ed98445e5836bd09bd89f64e34b809
PremNodejsdeveloper <premprakash1519@gmail.com>
2019-12-07 13:14:19 +0530

    changes in usermodel

--- api/models/users.js
+++ api/models/users.js
@@ -44,48 +44,48 @@ const userSchema = mongoose.Schema({
 
 let userModel= mongoose.model("Users", userSchema);
 //exporting the user model
-module.exports=userModel;
-// module.exports = {
-//     Users: userModel,
+//module.exports=userModel;
+module.exports = {
+    Users: userModel,
 
-//     createUser: async function(userDTO){
-//         //create a newUser object with the users provided data
-//        let newUser = new this.Users({
-//         firstName : userDTO.firstName,
-//         lastName  : userDTO.lastName,
-//         email     : userDTO.email,
-//         password  : userDTO.password,
-//         gender    : userDTO.gender,
-//         phone     : userDTO.phone,
+    createUser: async function(userDTO){
+        //create a newUser object with the users provided data
+       let newUser = new this.Users({
+        firstName : userDTO.firstName,
+        lastName  : userDTO.lastName,
+        email     : userDTO.email,
+        password  : userDTO.password,
+        gender    : userDTO.gender,
+        phone     : userDTO.phone,
 
-//        });
+       });
         
-//        try{
-//         let savedUser= await newUser.save();
-//         return savedUser;
-//        }catch(err){
-//         console.log("error occured creating user",err);
-//         return err;
-//        }      
-//     },
+       try{
+        let savedUser= await newUser.save();
+        return savedUser;
+       }catch(err){
+        console.log("error occured creating user",err);
+        return err;
+       }      
+    },
 
-//     updateUser: function(){
+    updateUser: function(){
 
-//     },
+    },
 
-//     findUserByEmail : function(paramEmail){
-//         //let registerdUser=
-//         this.Users.find({email:paramEmail})
-//         .then(function(err,data){
-//             if(err){
-//                 return err
-//             }else{
-//                 console.log("data==> ",data);
-//                 return data;
-//             }
-//         })
-//         //return registerdUser;
-//     }
-// }
+    findUserByEmail : function(paramEmail){
+        //let registerdUser=
+        this.Users.find({email:paramEmail})
+        .then(function(err,data){
+            if(err){
+                return err
+            }else{
+                console.log("data==> ",data);
+                return data;
+            }
+        })
+        //return registerdUser;
+    }
+}
 
 
--- api/services/userService.js
+++ api/services/userService.js
@@ -7,30 +7,17 @@ module.exports ={
     signUp: async function (userDTO) {
         try{
             let customeResponse
-            let existingUser =  Users.find({email:userDTO.email})
-            // .exec((error,data)=>{
-            //     if(error)
-            //     return error
-            //     else{
-              console.log("existingUser==>",existingUser);
-              
-            // //console.log("existingUser==>",existingUser);
-            // console.log("data=>  ",data);
-            //    return data;
-            // if (existingUser) {
-            //     customeResponse=buildResponse.errorResponse(402,"user already exists");
-            //     return customeResponse;
-            //     //return { status: 402, message: "user already exists" };
-            // } else {
-            //     // calling userModel methods to insert the data in database
-            //     //let userRecord = await userModel.createUser(userDTO);
-            //     return userRecord;
-            // }
-            // console.log( "data is ==> ",data);
-            //         return data;
-            //     }
-            // });
-            return existingUser;
+            let existingUser =  Users.find({email:userDTO.email});
+            if (existingUser) {
+                customeResponse=buildResponse.errorResponse(402,"user already exists");
+                return customeResponse;
+                //return { status: 402, message: "user already exists" };
+            } else {
+                // calling userModel methods to insert the data in database
+                //let userRecord = await userModel.createUser(userDTO);
+                return userRecord;
+            }
+            //return existingUser;
             
         }catch(error){
              customeResponse=buildResponse.errorResponse(500,"Some Exception Occured");
